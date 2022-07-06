9fb653df16af9136093fa204e6fb3190cd0ab57e
kmnb6842 <kmnb6842@gmail.com>
2022-07-06 18:09:01 +0800

    1246

---
+++ Blossom/app.js
@@ -0,0 +1,58 @@
+const intro = document.querySelector(".intro");
+const sub01 = document.querySelector(".sub01");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 5000,
+  triggerElement: intro,
+  triggerHook: 0
+
+})
+  // .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+
+
+//VIDEO ANIMATION
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+    scrollpos = e.scrollPos /300 ;
+    console.log(e);
+})
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    video.currentTime = scrollpos;
+}, 90);
+
+// FADE IN ANIMATION
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
